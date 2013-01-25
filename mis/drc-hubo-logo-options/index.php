911b8e5c98523838d9c4aa7529590d61280500a5
Daniel Lofaro <dan@danlofaro.com>
2013-01-24 17:36:44 -0800

    added new files and bmi stuff

---
+++ mis/drc-hubo-logo-options/index.php
@@ -0,0 +1,35 @@
+<?php
+	include '../../head.php';
+?>
+<?php
+	include '../../top.php';
+?>
+
+<div class="mainarea">
+
+<p class="text"><img src="/images/lofaroHubo.jpg" alt="Daniel M. Lofaro - Events" width="20%" height="20%" class="rightphoto" style="margin-top:20px;"/>
+<h2>drc-hubo logo options</h2>
+
+<h3>Option 1:</h3>
+<img src="drc-hubo_2012-11-21_1.png" />
+<hr></hr>
+<h3>Option 2:</h3>
+<img src="drc-hubo_2012-11-21_2.png" />
+<hr></hr>
+<h3>Option 3:</h3>
+<img src="drc-hubo_2012-11-21_3.png" />
+<hr></hr>
+<h3>Option 4:</h3>
+<img src="drc-hubo_2012-11-21_4.png" />
+<hr></hr>
+<h3>Option 5:</h3>
+<img src="drc-hubo_2012-11-21_5.png" />
+<hr></hr>
+<h3>Option 6:</h3>
+<img src="drc-hubo_2012-11-21_6.png" />
+
+
+<?php
+	include '../../foot.php';
+?>
+
---
+++ projects/bmi/index.php
@@ -0,0 +1,23 @@
+<?php
+	include '../../head.php';
+?>
+<?php
+	include '../../top.php';
+?>
+
+<div class="mainarea">
+
+<p class="text"><img src="../../images/lofaroHubo.jpg" alt="Daniel M. Lofaro" width="20%" height="20%" class="rightphoto" style="margin-top:20px;"/>
+
+
+
+<p>
+<h2>Robot Control Using 14 Electrode BMI System</h2>
+<iframe width="420" height="315" src="http://www.youtube.com/embed/mg-Qv3SxU94" frameborder="0" allowfullscreen></iframe>
+</p>
+</p>
+
+<?php
+	include '../../foot.php';
+?>
+
