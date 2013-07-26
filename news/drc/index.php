984331a6d82921fad41706a3c4f6e8c06bc370be
Daniel Lofaro <dan@danlofaro.com>
2013-07-26 14:07:32 -0700

    added news/drc

---
+++ news/drc/index.php
@@ -0,0 +1,43 @@
+<?php
+	include '../../head.php';
+?>
+<?php
+	include '../../top.php';
+?>
+
+<div class="mainarea">
+
+<p class="text"><img src="../../images/lofaroHubo.jpg" alt="Daniel M. Lofaro" width="20%" height="20%" class="rightphoto" style="margin-top:20px;"/>
+
+<h2>DRC-HUBO Media Coverage (October 2012- July 2013)</h2>
+<hr></hr>
+<p>
+<ul>
+<li><a href="http://www.npr.org/player/v2/mediaPlayer.html?action=1&amp;id=163718568&amp;m=163730141">National Public Radio (audio), October 26, 2012</a></li>
+<li><a href="http://www.npr.org/blogs/alltechconsidered/2012/10/26/163718568/a-contest-to-build-a-disaster-ready-robot">National Public Radio (online), October 26, 2012</a></li>
+<li><a href="http://arstechnica.com/science/2012/10/darpas-next-grand-challenge-tool-wielding-rescue-robots/">Ars Technica, October 25, 2012</a></li>
+<li><a href="http://www.geekologie.com/2012/10/were-all-dead-darpas-human-like-robot-le.php">Geekologie, October 25, 2012 </a></li>
+<li><a href="http://www.gizmag.com/darpa-robotics-challenge-humanoid-robots/24722/">Gizmag, October 25, 2012 </a></li>
+<li><a href="http://www.newscientist.com/blogs/onepercent/2012/10/darpa-robot-challenge.html">New Scientist, October 25, 2012 </a></li>
+<li><a href="http://bits.blogs.nytimes.com/2012/10/24/in-contest-for-rescue-robots-darpa-offers-2-million-prize/">New York Times, October 24, 2012</a></li>
+<li><a href="http://www.pcmag.com/slideshow/story/304273/darpa-wants-robotics-to-rise-to-the-challenge-of-disasters">PC Mag, October 24, 2012</a></li>
+<li><a href="http://www.wired.com/dangerroom/2012/10/darpa-rescue-robot/">Wired.com, October 24, 2012</a></li>
+<li><a href="http://spectrum.ieee.org/automaton/robotics/humanoids/iros-2012-darpa-robotics-challenge-update">IEEE Spectrum, October 24, 2012</a></li>
+<li><a href="http://technicallyphilly.com/2012/10/25/drexel-to-build-a-robot-that-can-respond-to-a-radioactive-disaster-darpa-robotics-challenge#more-17494">Technically Philly, October 24, 2012</a></li>
+<li><a href="http://www.nbcnews.com/technology/futureoftech/human-robots-getting-ready-save-lives-1C6663298">NBCNews.com, October 24, 2012</a></li>
+<li><a href="http://www.philly.com/philly/news/nation_world/20121127_Drexel_team_competing_to_build_robot_for_nuclear_emergencies.html">Philadelphia Inquirer, November 27, 2012</a></li>
+<li><a href="http://www.popsci.com/technology/article/2013-01/how-build-hero?page=4">Popular Science, February 1, 2013</a></li>
+<li><a href="http://www.youtube.com/watch?feature=player_embedded&amp;v=7rY3PCLpmP0">NHK-TV, March 17, 2013</a></li>
+<li><a href="http://www.designnews.com/author.asp?section_id=1386&amp;doc_id=260603&amp;piddl_msgpage=1">Design News, March 21, 2013</a></li>
+<li><a href="http://s3.amazonaws.com/TVEyesMediaCenter/UserContent/228406/1796238.4208/WHYY_04-07-2013_16.10.42.mp4">WHYY "Friday Arts," April 5, 2013</a></li>
+<li><a href="http://us.cnn.com/2013/05/09/tech/innovation/robot-humanoid-baltimore-robocop/index.html">CNN.com, May 9, 2013</a></li>
+<li><a href="http://www.bloomberg.com/video/meet-atlas-darpa-s-startling-humanoid-future-vRjkAPRbSUaNO~~BF15K0w.html">Bloomberg News, July 22, 2013</a></li>
+<li><a href="http://www.redorbit.com/news/technology/1112896115/darpa-humanoid-robot-atlas-debuts-robotics-challenge-0711213/">Redorbit, July 12, 2013</a></li>
+</ul>
+
+</p>
+
+<?php
+	include '../../foot.php';
+?>
+
