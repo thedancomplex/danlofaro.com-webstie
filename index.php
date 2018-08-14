eae7fb6e3a1eca0bee72a8c15cd88ede96f50b57
Dan Lofaro <geovana@prince-george.dreamhost.com>
2018-08-13 19:34:13 -0700

    adding new front page

--- index.php
+++ index.php
@@ -7,14 +7,29 @@
 
 <div class="mainarea">
 
-<p class="text"><img src="images/lofaroHubo.jpg" alt="Daniel M. Lofaro" width="20%" height="20%" class="rightphoto" style="margin-top:20px;"/><h2>Home</h2><p class="text">Daniel M. Lofaro Ph.D. is an Assistant Professor at George Mason University in Fairfax, Virginia, USA.  
+<p class="text"><img src="images/lofaroHubo.jpg" alt="Daniel M. Lofaro" width="20%" height="20%" class="rightphoto" style="margin-top:20px;"/><h2>Home</h2><p class="text">
+Daniel M. Lofaro is an Assistant Professor in the Electrical and Computer Engineering Department at George Mason University. He is the director of Lofaro Labs Robotics and the Drones and Autonomous Systems Lab @George Mason University (DASL@GMU). An NSF-EAPSI and ONR-SFRP Fellow, he received his doctorate from the ECE Department at Drexel University in 2013. From 2012-2013 he was the Research Lead of the DARPA Robotic Challenge team DRC-Hubo. His research interests include Complex Control Systems and Robotics with most recent ventures relating to Robot Design and Cloud Robotics.
+
+<!--
+Daniel M. Lofaro Ph.D. is an Assistant Professor at George Mason University in Fairfax, Virginia, USA.  
+
+
 A graduate from the Electrical and Computer Engineering Department at Drexel University, he worked at the Drexel Autonomous Systems Lab (DASL) at under Dr. Paul Oh.
 Research interests include <i>Complex Control Systems and Robotics</i> with most recent ventures relating to <i>Robot Design</i> and <i>Cloud Robotics</i>.  
+-->
+
 Daniel's dissertation title is <i><a href="phd/">Unified Algorithmic Framework for High Degree of Freedom Complex Systems and Humanoid Robots</a></i>.  Recent achievements include:
 </p></p>
 <ul class="text">
+  <li>U.S. Naval Research Laboratory (NRL)- NCARAI - Affiliate Faculty, 2017 - Present</li>
+  <li>Grant: National Science Foundation (NSF) - Doctoral Consortium at the 2017 IEEE/RSJ IROS, 2017</li>
+  <li>Grant: Amazon Inc. - Autonomous Robotic Manipulation and Stowing System for Warhorse Settings, 2017</li>
+  <li>Grant: IEEE - SIGHT Project-Smart Hive, 2017</li>
+  <li>Office of Naval Research - Summer Faculty Research Program (ONR-SFRP) Recipient, 2016</li>
+  <li>Office of Naval Research - Summer Faculty Research Program (ONR-SFRP) Recipient, 2015</li>
   <li><a href="http://www.darpa.mil/Our_Work/TTO/Programs/DARPA_Robotics_Challenge.aspx">DARPA Robotics Challenge (DRC):</a> Team <a href="http://www.drc-hubo.com/">DRC-Hubo</a> Research Lead, 2013</li>
   <li>Doctoral recipient, Electrical and Computer Engineering Department at Drexel University, 2013</li>
+  <li>Dissertation title:<i><a href="phd/">Unified Algorithmic Framework for High Degree of Freedom Complex Systems and Humanoid Robots</a></i></li>
   <li>Designed Hubo to Throw First Pitch at Major League Baseball Game, 2012<br /></li>
   <li>NSF-Graduate Research Fellowship Program (GRFP) Honorable Mention, 2009<br /></li>
   <li>NSF-East Asia and Pacific Summer Institutes (EAPSI) Fellow, 2008<br /></li>
